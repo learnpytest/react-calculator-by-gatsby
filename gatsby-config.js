0f400d01bf2e477506a93facf4649ae9cde39e3b
RachelChen <iceage91@gmail.com>
2022-11-27 13:03:11 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `react-calculator-by-gatsby`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
