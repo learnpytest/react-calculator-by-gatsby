1af3fc6fbd269e21436bea6e4a401fe07e305136
RachelChen <iceage91@gmail.com>
2022-11-27 18:30:47 +0800

    adjust css font alignment and other structure

--- src/components/calculator.component.jsx
+++ src/components/calculator.component.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { updateEquation, updateResult, clearAll } from "../functions/calculator.function"
+import {
+  updateEquation,
+  updateResult,
+  clearAll,
+} from "../functions/calculator.function";
 
 // styles
 const RcContainerStyle = styled.div`
@@ -26,10 +30,11 @@ const RcContentStyle = styled.div`
 const HatchStyle = styled.div`
   padding-top: 100%;
   position: relative;
-  @media screen and (hover: hover) {
-    button:hover {
-      background-color: var(--hover-red);
-    }
+  &:nth-child(3) span {
+    transform: translateY(-1px);
+  }
+  &:nth-child(4) span {
+    transform: translateY(6px);
   }
 `;
 
@@ -48,6 +53,11 @@ const ButtonStyle = styled.button`
   background-color: rgba(255, 255, 255, 0.55);
   font-size: 1.5rem;
   color: var(--grey-extra-dark);
+  @media screen and (hover: hover) {
+    &:hover {
+      background-color: var(--hover-red);
+    }
+  }
 `;
 
 const DisplayStyle = styled.div`
@@ -60,103 +70,137 @@ const TextStyle = styled.div`
   width: 100%;
   font-family: "IBM Plex Mono";
   font-size: 1.5rem;
-  input {
-    width: 100%;
-    border: none;
-    background-color: inherit;
-    box-sizing: border-box;
-    line-height: 1.5;
-    text-align: right;
-    color: var(--grey-extra-dark);
-  }
-  input:focus {
-    outline: 1px solid var(--main-blue-green);
-  }
 `;
 const ResultStyle = styled.div`
   width: 100%;
   border-bottom: 1px solid var(--main-blue-green);
 `;
 
-const ExceedLimit = styled.span`
+const ExceedLimitStyle = styled.span`
   color: var(--main-purple);
+`;
+
+const InputStyle = styled.input`
+  width: 100%;
+  border: none;
+  background-color: inherit;
+  box-sizing: border-box;
+  line-height: 1.5;
+  text-align: right;
+  color: var(--grey-extra-dark);
+  &:focus {
+    outline: 1px solid var(--main-blue-green);
+  }
 `;
 
 // Component variables
 const LIMIT = Math.pow(2, 32);
 const OPERATORS = ["+", "-", "*", "/"];
 const getCalcState = () => ({
-    equation: "",
-    result: null,
-    isExceedLimit: false,
+  equation: "",
+  result: null,
+  isExceedLimit: false,
 });
 
 // Component function
 const handleCalculator = {
-    "UPDATE_EQUATION": (input, calc, setCalc, limit, type, isTargetValid) => updateEquation(input, calc, setCalc, limit, type, isTargetValid),
-    "UPDATE_RESULT": (calc, setCalc, operators, LIMIT) => updateResult(calc, setCalc, operators, LIMIT),
-    "CLEAR_ALL": (calcState, setCalc) => clearAll(calcState, setCalc)
-}
+  UPDATE_EQUATION: (input, calc, setCalc, limit, type, isTargetValid) =>
+    updateEquation(input, calc, setCalc, limit, type, isTargetValid),
+  UPDATE_RESULT: (calc, setCalc, operators, LIMIT) =>
+    updateResult(calc, setCalc, operators, LIMIT),
+  CLEAR_ALL: (calcState, setCalc) => clearAll(calcState, setCalc),
+};
 
-const isTargetValid = (target1, blackList1, target2, blackList2=[]) => {
-    if (target2 === undefined) {
-        return !blackList1.includes(target1)
-    }
-    return !blackList1.includes(target1) || !blackList2.includes(target2)
-}
+const isTargetValid = (target1, blackList1, target2, blackList2 = []) => {
+  if (target2 === undefined) {
+    return !blackList1.includes(target1);
+  }
+  return !blackList1.includes(target1) || !blackList2.includes(target2);
+};
 
 const CalculatorComponent = () => {
   const calcState = getCalcState();
   const [calc, setCalc] = useState(calcState);
-  
+
   return (
     <RcContainerStyle>
       <RcContentStyle>
         <DisplayStyle>
           <TextStyle>
             <ResultStyle>
-              <input
+              <InputStyle
                 type="text"
                 readOnly
-                value={`Ans: ${
-                  calc.result || 0
-                }`}
+                value={`Ans: ${calc.result || 0}`}
               />
             </ResultStyle>
           </TextStyle>
           <TextStyle>
-            <input
-              type="text"
-              readOnly
-              value={
-                calc.equation || 0
-              }
-            />
+            <InputStyle type="text" readOnly value={calc.equation || 0} />
           </TextStyle>
-          {calc.isExceedLimit && <ExceedLimit>超過數字上限</ExceedLimit>}
+          {calc.isExceedLimit && (
+            <ExceedLimitStyle>超過數字上限</ExceedLimitStyle>
+          )}
         </DisplayStyle>
         {OPERATORS.map((operator, index) => {
           return (
             <HatchStyle key={index}>
-                  <ButtonStyle onClick={() => handleCalculator["UPDATE_EQUATION"](operator, calc, setCalc, LIMIT, "operator", isTargetValid(calc.equation.slice(-1), OPERATORS))}>
-                <span class={`operator-text-${index}`}>{operator}</span>
+              <ButtonStyle
+                onClick={() =>
+                  handleCalculator["UPDATE_EQUATION"](
+                    operator,
+                    calc,
+                    setCalc,
+                    LIMIT,
+                    "operator",
+                    isTargetValid(calc.equation.slice(-1), OPERATORS)
+                  )
+                }
+              >
+                <span>{operator}</span>
               </ButtonStyle>
             </HatchStyle>
           );
         })}
         <HatchStyle>
-                  <ButtonStyle onClick={() => handleCalculator["CLEAR_ALL"](calcState, setCalc)}>C</ButtonStyle>
+          <ButtonStyle
+            onClick={() => handleCalculator["CLEAR_ALL"](calcState, setCalc)}
+          >
+            C
+          </ButtonStyle>
         </HatchStyle>
         <HatchStyle>
-                  <ButtonStyle onClick={() => handleCalculator["UPDATE_RESULT"](calc, setCalc, OPERATORS, LIMIT)}>=</ButtonStyle>
+          <ButtonStyle
+            onClick={() =>
+              handleCalculator["UPDATE_RESULT"](calc, setCalc, OPERATORS, LIMIT)
+            }
+          >
+            =
+          </ButtonStyle>
         </HatchStyle>
         {Array(10)
           .fill(0)
           .map((_, index) => {
             return (
               <HatchStyle key={index}>
-                    <ButtonStyle onClick={() => handleCalculator["UPDATE_EQUATION"](index, calc, setCalc, LIMIT, "digit", isTargetValid(calc.equation.slice(-1), ["0"], calc.equation.slice(-2, -1), OPERATORS))}>
-                  <span class={`digit-text-${index}`}>{index}</span>
+                <ButtonStyle
+                  onClick={() =>
+                    handleCalculator["UPDATE_EQUATION"](
+                      index,
+                      calc,
+                      setCalc,
+                      LIMIT,
+                      "digit",
+                      isTargetValid(
+                        calc.equation.slice(-1),
+                        ["0"],
+                        calc.equation.slice(-2, -1),
+                        OPERATORS
+                      )
+                    )
+                  }
+                >
+                  <span>{index}</span>
                 </ButtonStyle>
               </HatchStyle>
             );
