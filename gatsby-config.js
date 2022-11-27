d5a2d563fd5b5bff71748e10af0e1f680055170b
RachelChen <iceage91@gmail.com>
2022-11-27 14:22:39 +0800

    modified metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,31 @@
 module.exports = {
   siteMetadata: {
     title: `react-calculator-by-gatsby`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://github.com/learnpytest/-react-calculator-by-gatsby`,
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `react-calculator-by-gatsby`,
+        short_name: `rc-calculator`,
+        start_url: `/`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+        icons: [
+          {
+            src: `/favicons/favicon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/favicon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+  ],
 };
